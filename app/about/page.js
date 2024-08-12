36c4a0140c87ca546584eebb4fb45943b746f04e
jobex11 <joblawal33@gmail.com>
2024-08-12 16:33:40 +0100

    about responsiveness

--- app/about/page.js
+++ app/about/page.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 import Image from "next/image";
 const page = () => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full overflow-hidden flex flex-col">
       <div className="flex justify-center items-center bg-custom-bg-1 h-[300px] w-full p-4">
-        <h1 className="font-bold w-[30%] text-gray-50 text-3xl">
+        <h1 className="font-bold w-[60%] md:w-[30%] text-gray-50 text-xl md:text-3xl">
           Revolutionizing access to Pharmacists and Medicine
         </h1>
       </div>
-      <div className="w-inherit py-10 px-4 text-primary">
-        <div className="bg-custom-bg-cyan rounded-xl p-12 flex flex-col items-center">
-          <h2 className="font-bold text-xl my-4">Our Story</h2>
+      <div className="w-inherit py-5  md:py-10 px-4 text-primary">
+        <div className="bg-custom-bg-cyan rounded-xl p-6 md:p-12 flex flex-col items-center">
+          <h2 className="font-bold text-xl my-2 md:my-4">Our Story</h2>
           <h3 className="text-sm">
             <h3 className="my-3">
               ll we wanted to do was create, multiply, and offer impact. What we
@@ -51,8 +51,8 @@ const page = () => {
         </div>
       </div>
       {/* third section */}
-      <div className="w-full flex justify-center space-x-6 text-sm text-gray-600 my-6">
-        <div className="w-[20%]">
+      <div className="w-full md:flex justify-center  md:space-x-6 text-sm text-gray-600 my-2 md:my-6">
+        <div className="md:w-[20%] text-center md:text-start py-2 md:py-0">
           <hr />
           <h2 className="text-base font-bold py-2">The Vision</h2>
           <h3>
@@ -60,7 +60,7 @@ const page = () => {
             Healthier Africa)
           </h3>
         </div>
-        <div className="w-[20%]">
+        <div className="md:w-[20%] text-center md:text-start py-2 md:py-0">
           <hr />
           <h2 className="text-base font-bold py-2">Our Mission</h2>
           <h3>
@@ -68,7 +68,7 @@ const page = () => {
             affordable healthcare
           </h3>
         </div>
-        <div className="w-[20%]">
+        <div className="md:w-[20%] text-center md:text-start py-2 md:py-0">
           <hr />
           <h2 className="text-base font-bold py-2">Our Core Values</h2>
           <h3>
@@ -79,19 +79,19 @@ const page = () => {
       </div>
       {/* fourth section*/}
 
-      <div className="w-full py-10 px-4 text-primary">
-        <div className="bg-[#F0F7FF] text-sm mt-10 rounded-xl p-12">
+      <div className="w-full py-1 md:py-10 px-4 text-primary">
+        <div className="bg-[#F0F7FF] text-sm mt-2 md:mt-10 rounded-xl p-3 md:p-12">
           <div className="flex flex-col justify-center items-center">
-            <h2 className="font-bold text-xl">The Founding Team</h2>
-            <h3 className="w-[40%] text-center">
+            <h2 className="font-bold text-lg md:text-xl">The Founding Team</h2>
+            <h3 className="md:w-[40%] text-center">
               To provide these services, Flolog deploys the right technology,
               the right team, and at the right time always. The management team
               is led by three stellar Pharmacists
             </h3>
           </div>
-          <div className="mt-10 flex justify-center items-center space-x-12">
-            <div className="text-center">
-              <div className="border w-[220px] h-[250px]">
+          <div className="mt-6 md:mt-10 md:flex justify-center items-center md:space-x-12">
+            <div className="my-6 md:my-0  flex flex-col items-center md:block text-center">
+              <div className="border w-[120px] md:w-[220px] h-[150px] md:h-[250px]">
                 <Image
                   src="/images/Rectangle93.png"
                   width={100}
@@ -101,17 +101,23 @@ const page = () => {
                   className="w-full h-full"
                 />
               </div>
-              <h3 className="py-1 text-lg font-bold">Andrew Akhabue</h3>
+              <h3 className="md:py-1 text-sm md:text-lg font-bold">
+                Andrew Akhabue
+              </h3>
               <h5 className="text-sm">Co-founder and CEO</h5>
             </div>
-            <div className="text-center">
-              <div className="border w-[220px] h-[250px]"></div>
-              <h3 className="py-1 text-lg font-bold">Raphael Odoh</h3>
+            <div className="my-6 md:my-0 flex flex-col items-center md:block text-center">
+              <div className="border w-[120px] md:w-[220px] h-[150px] md:h-[250px]"></div>
+              <h3 className="md:py-1 text-sm md:text-lg font-bold">
+                Raphael Odoh
+              </h3>
               <h5 className="text-sm">Chief Operations Officer</h5>
             </div>
-            <div className="text-center">
-              <div className="border w-[220px] h-[250px]"></div>
-              <h3 className="py-1 text-lg font-bold">Joy Enwerem</h3>
+            <div className="my-6 md:my-0 flex flex-col items-center md:block text-center">
+              <div className="border w-[120px] md:w-[220px] h-[150px] md:h-[250px]"></div>
+              <h3 className="md:py-1 text-sm md:text-lg font-bold">
+                Joy Enwerem
+              </h3>
               <h5 className="text-sm">COO Finance and Strategy</h5>
             </div>
           </div>
@@ -119,14 +125,46 @@ const page = () => {
       </div>
 
       {/*fifth section*/}
-      <div className="w-full h-full text-primary">
+      <div className="w-full md:h-full text-primary">
         <div className="w-full flex flex-col items-center">
           <h5 className="py-4 text-sm font-bold">OUR STRATEGY</h5>
           <h3 className="font-bold">Senior Citizens Care Program</h3>
           <h3 className="font-bold">(SECCAP)</h3>
         </div>
-        <div className="px-6">
-          <div className="flex justify-center space-x-4 my-4 w-full h-[200px]">
+        <div className="px-3 md:px-6">
+          <div className="md:flex justify-center space-y-2 md:space-y-0 md:space-x-4 my-4 w-full md:h-[200px]">
+            <div className="md:w-[20%]">
+              <Image
+                src="/Rectangle89.png"
+                height={100}
+                width={100}
+                quality={100}
+                unoptimized={true}
+                className="w-full h-full"
+              />
+            </div>
+            <div className="md:w-[20%]">
+              <Image
+                src="/Rectangle90.png"
+                height={100}
+                width={100}
+                quality={100}
+                unoptimized={true}
+                className="w-full h-full"
+              />
+            </div>
+            <div className="md:w-[20%]">
+              <Image
+                src="/Rectangle91.png"
+                height={100}
+                width={100}
+                quality={100}
+                unoptimized={true}
+                className="w-full h-full"
+              />
+            </div>
+          </div>
+          <div className="hidden md:flex justify-start space-x-4 my-4 w-full h-[200px]">
             <div className="w-[20%]">
               <Image
                 src="/Rectangle89.png"
@@ -158,39 +196,7 @@ const page = () => {
               />
             </div>
           </div>
-          <div className="flex justify-start space-x-4 my-4 w-full h-[200px]">
-            <div className="w-[20%]">
-              <Image
-                src="/Rectangle89.png"
-                height={100}
-                width={100}
-                quality={100}
-                unoptimized={true}
-                className="w-full h-full"
-              />
-            </div>
-            <div className="w-[20%]">
-              <Image
-                src="/Rectangle90.png"
-                height={100}
-                width={100}
-                quality={100}
-                unoptimized={true}
-                className="w-full h-full"
-              />
-            </div>
-            <div className="w-[20%]">
-              <Image
-                src="/Rectangle91.png"
-                height={100}
-                width={100}
-                quality={100}
-                unoptimized={true}
-                className="w-full h-full"
-              />
-            </div>
-          </div>
-          <div className="flex justify-end space-x-4 my-4 w-full h-[200px]">
+          <div className="hidden md:flex justify-end space-x-4 my-4 w-full h-[200px]">
             <div className="w-[20%]">
               <Image
                 src="/Rectangle89.png"
@@ -223,8 +229,8 @@ const page = () => {
             </div>
           </div>
         </div>
-        <div className="text-gray-100 text-base bg-custom-bg-6 h-full w-full flex flex-col justify-center items-center py-10">
-          <h3 className="w-[40%]">
+        <div className="text-gray-100 px-3  md:px-0 text-base bg-custom-bg-6 md:h-full w-full flex flex-col justify-center items-center py-5 md:py-10">
+          <h3 className="md:w-[40%]">
             “In Trust, Innovation and Excellence, we exist to provide unhindered
             access to healthcare for every African”           
           </h3>
